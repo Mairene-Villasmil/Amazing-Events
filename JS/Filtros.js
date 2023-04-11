f9a08e2fe2165b5ef946483c0ad301842ed815f8
Mairene-Villasmil <mairenevillasmilf@gmail.com>
2023-04-11 14:04:43 -0300

    rutas listas

--- JS/Filtros.js
+++ JS/Filtros.js
@@ -8,6 +8,9 @@ console.log(eventos);
 const eventosPasados = []
 const eventosFuturos = []
 
+console.log(eventosFuturos);
+console.log(eventosFuturos);
+
 for (var i = 0; i < eventos.length; i++) {
 
     if (eventos[i].date > fechaBase) {
@@ -18,37 +21,39 @@ for (var i = 0; i < eventos.length; i++) {
 }
 
 
+// var buttonNavegar = []
 var buttonNav = document.getElementsByClassName("nav-item")
 for (var i = 0; i < buttonNav.length; i++) {
     const element = buttonNav[i];
+    // buttonNavegar.push(buttonNav[i].innerText)
     element.addEventListener("click", function (e) {
         imprimir(e.target.id);
-        
+
     })
 }
+
+// console.log(buttonNavegar)
 
 function imprimir(id) {
 
     switch (id) {
 
         case "upcoming":
-            document.getElementById("tituloEncabezado").innerHTML = "Eventos Futuros"
-            document.getElementById("navegacion-titulo").classList.add('nav_home')
-            display(eventosFuturos)
-            container.innerHTML =""
+            document.getElementById("tituloEncabezado").innerHTML = "Eventos Futuros";
+            document.getElementById("navegacion-titulo").classList.add('nav_home');
+            display(eventosFuturos);
             break;
 
         case "past":
-            document.getElementById("tituloEncabezado").innerHTML = "Eventos Pasados"
-            document.getElementById("navegacion-titulo").classList.add('nav_home')
-            display(eventosPasados)
-            container.innerHTML =""
+            document.getElementById("tituloEncabezado").innerHTML = "Eventos Pasados";
+            document.getElementById("navegacion-titulo").classList.add('nav_home');
+            display(eventosPasados);
             break;
 
         case "contact":
-            document.getElementById("tituloEncabezado").innerHTML = "Contacta con Nosotros"
-            document.getElementById("navegacion-titulo").classList.add('nav_home')
-            container.innerHTML =`
+            document.getElementById("tituloEncabezado").innerHTML = "Contacta con Nosotros";
+            document.getElementById("navegacion-titulo").classList.add('nav_home');
+            tarjetas.innerHTML = `
             <section class="formulario">
                 <form class="formu">
                     <div class="formulario_dos"> 
@@ -63,14 +68,90 @@ function imprimir(id) {
                 </form>
             </section>
             `
-            tarjetas.innerHTML="";
+            break;
+
+        case "stats":
+            document.getElementById("tituloEncabezado").innerHTML = "Estadísticas";
+            document.getElementById("navegacion-titulo").classList.add('nav_home');
+            tarjetas.innerHTML = `
+            <table>
+            <tr class="color">
+              <th colspan="3">Estadísticas de Eventos</th>
+            </tr>
+            <tr class="titulo">
+              <th>Eventos con Mayor Porcentaje de Asistencia</th>
+              <th>Eventos con Menor Porcentaje de Asistencia</th>
+              <th>Eventos de Mayor Capacidad</th>
+            </tr>
+            <tr>
+              <td>Metallica en Concierto</td>
+              <td>Fiesta de Disfraces</td>
+              <td>Metallica en Concierto</td>
+            </tr>
+            <tr>
+              <td>Feria del libro Escolar</td>
+              <td>Avengers</td>
+              <td>Feria del libro Escolar</td>
+            </tr>
+            <tr class="color">
+              <th colspan="3">Estadisticas de Eventos Próximos por Catgoría</th>
+            </tr>
+            <tr class="titulo">
+              <th>Categorías</th>
+              <th>Ingresos</th>
+              <th>Porcentaje de Estimación</th>
+            </tr>
+            <tr>
+              <td>Metallica en Concierto</td>
+              <td>Concierto de Música</td>
+              <td>138.000</td>
+            </tr>
+            <tr>
+              <td>Noche de Halloween</td>
+              <td>Fiesta de Disfraces</td>
+              <td>9.000</td>
+            </tr>
+            <tr>
+              <td>Avengers</td>
+              <td>Vamos al Cine</td>
+              <td>9.000</td>
+            </tr>
+            <tr class="color">
+              <th colspan="3">Estadisticas de Eventos Pasados por Catgoría</th>
+            </tr>
+            <tr class="titulo">
+              <th>Categorías</th>
+              <th>Ingresos</th>
+              <th>Porcentaje de Asistencia</th>
+            </tr>
+            <tr>
+              <td>10K por la vida</td>
+              <td>Carrera</td>
+              <td>25.698</td>
+            </tr>
+            <tr>
+              <td>Feria del libro Escolar</td>
+              <td>Intercambio de Libros</td>
+              <td>123.286</td>
+            </tr>
+            <tr>
+              <td>Parque Jurásico</td>
+              <td>Salida al Museo</td>
+              <td>65.892</td>
+            </tr>
+            <tr>
+              <td>Fiesta de las Colectividades</td>
+              <td>Feria de Comida</td>
+              <td>42.756</td>
+            </tr>
+          </table>
+            `
             break;
 
         default:
-            document.getElementById("tituloEncabezado").innerHTML = "Inicio"
-            document.getElementById("navegacion-titulo").classList.add('nav_home')
-            display(eventos)
-            container.innerHTML =""
+            document.getElementById("tituloEncabezado").innerHTML = "Inicio";
+            document.getElementById("navegacion-titulo").classList.add('nav_home');
+            display(eventos);
 
     }
 
@@ -101,7 +182,7 @@ function display(array) {
 
 }
 
-imprimir("home");
+
 
 var time = location.search.split("?time=")
 console.log(time);
@@ -118,7 +199,10 @@ switch (time[1]) {
 
     case "contact":
         imprimir("contact")
-        
+        break;
+
+    case "stats":
+        imprimir("stats")
         break;
 
     default:
@@ -126,3 +210,35 @@ switch (time[1]) {
 
 }
 
+
+const arrowD = document.getElementById("flechaDerecha");
+arrowD.addEventListener("click", function (e) {
+    flechas(e.target.pag)
+})
+const arrowI = document.getElementById("flechaIzquierda");
+arrowI.addEventListener("click", function (e) {
+    flechas(e.target.pag)
+    console.log(e);
+})
+
+function flechas(pag) {
+    if (time[1] == "upcoming") {
+        arrowD.href = "/index.html?time=past"
+        arrowI.href = "/index.html"
+    } else
+        if (time[1] == "past") {
+            arrowD.href = "/index.html?time=contact"
+            arrowI.href = "/index.html?time=upcoming"
+        } else
+            if (time[1] == "contact") {
+                arrowD.href = "/index.html?time=stats"
+                arrowI.href = "/index.html?time=past"
+            } else
+                if (time[1] == "stats") {
+                    arrowD.href = "/index.html"
+                    arrowI.href = "/index.html?time=contact"
+                } else {
+                    arrowD.href = "/index.html?time=upcoming"
+                    arrowI.href = "/index.html?time=stats"
+                }
+}
